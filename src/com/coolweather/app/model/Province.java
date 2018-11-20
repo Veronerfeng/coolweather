33a891e62b60f69c59b1750ce63108c52404d9e3
veronerfeng <1120769978@qq.com>
2018-11-20 10:20:19 +0800

    CoolWeatherDB

---
+++ src/com/coolweather/app/model/Province.java
@@ -0,0 +1,25 @@
+package com.coolweather.app.model;
+
+public class Province {
+	private int id;
+	private String procinceName;
+	private String provinceCode;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getProcinceName() {
+		return procinceName;
+	}
+	public void setProcinceName(String procinceName) {
+		this.procinceName = procinceName;
+	}
+	public String getProvinceCode() {
+		return provinceCode;
+	}
+	public void setProvinceCode(String provinceCode) {
+		this.provinceCode = provinceCode;
+	}
+}
